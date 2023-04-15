2549422e2ef50ebdc035af49b7134f9efdeb471e
Wilhem Nédan <wilhem.nedan@gmail.com>
2023-04-15 16:52:54 -0400

    Add a class user

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,12 +1,12 @@
 import pool from '../db.js';
 import bcrypt from 'bcrypt';
 import queries from '../queries/queries.js';
+import classUser from '../classes/class-user.js';
 
 const getUsers = async(req,res) => {
     try {
         const users = await pool.query(queries.selectUsers);
         res.json({users: users.rows});
-        
     } catch (error) {
         res.status(500).json({error:error.message});
     }
@@ -14,12 +14,12 @@ const getUsers = async(req,res) => {
 const createUser = async(req,res) => {
     try {
         const {name,email,password} = req.body;
-        const hashedPassword = await bcrypt.hash(password,10);
-        const newUser = await pool.query(queries.createUser,[name,email,hashedPassword]);
+        const newClassUser = new classUser(name,email,password) 
+        const hashedPassword = await bcrypt.hash(newClassUser.password,10);
+        const newUser = await pool.query(queries.createUser,[newClassUser.name,newClassUser.email,hashedPassword]);
         res.json({users:newUser.rows[0]});
     } catch (error) {
         res.status(500).json({error:error.message});
-        
     }
 };
 
