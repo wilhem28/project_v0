f7390f8566b297dd296ddbc170cd243ccd9bb66e
Wilhem Nédan <wilhem.nedan@gmail.com>
2023-04-16 03:31:34 -0400

    Create a refresh token

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -1,6 +1,7 @@
 import pool from '../db.js';
 import bcrypt from 'bcrypt';
 import queries from '../queries/queries.js';
+import jwt from 'jsonwebtoken';
 import jwtTokens from './utils/jwt-helpers.js';
 
 const loginUser = async (req,res) => {
@@ -13,7 +14,7 @@ const loginUser = async (req,res) => {
         const validPassword = await bcrypt.compare(password,user.rows[0].user_password);
         if(!validPassword) return res.status(401).json({error : "Password is incorrect"});
 
-        let tokens = jwtTokens(user.rows[0]); console.log(tokens);
+        let tokens = jwtTokens(user.rows[0]);
         res.cookie('refresh_token',tokens.refreshToken,{httpOnly:true});
         res.json(tokens);
 
@@ -22,4 +23,19 @@ const loginUser = async (req,res) => {
     }
 };
 
-export default {loginUser};
+const refrehTokenUser = (req,res) => {
+    try {
+        const refreshToken = req.cookies.refresh_token;
+        if(refreshToken === null) return res.status(401).json({eror:'Refresh token is null'});
+        jwt.verify(refreshToken,process.env.REFRESH_TOKEN_SECRET, (error,user) => {
+            if(error) return res.status(403).json({error:error.message});
+            let tokens = jwtTokens(user);
+            res.cookie('refresh_token',tokens.refreshToken,{httpOnly:true});
+            res.json(tokens);
+        })
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+}
+
+export default {loginUser,refrehTokenUser};
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -5,5 +5,6 @@ import authControllers from '../controllers/auth-controllers.js';
 const router = express.Router();
 
 router.post('/login', authControllers.loginUser);
+router.get('/refresh_token',authControllers.refrehTokenUser);
 
 export default router;
