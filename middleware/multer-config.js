49961f65b629611b3dd33c976dafdb030be85d1e
Wilhem Nédan <wilhem.nedan@gmail.com>
2023-04-29 07:06:43 -0400

    Handle multer-config file second step

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -8,35 +8,43 @@ const MIME_TYPES = {
   'image/png': 'png',
   'image/webp': 'webp'
 },
-dir = './images'
+dir = './public/posts/images'
 ;
 let storage = "";
 
-
-if(!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
-    storage = multer.diskStorage({
-        destination: (req, file, callback) => {
-            callback(null, 'images');
-        },
-        filename: (req, file, callback) => {
-            const name = file.originalname.split(' ').join('_');
-            const extension = MIME_TYPES[file.mimetype];
-            callback(null, name + Date.now() + '.' + extension);
-        }
-    });
-} else {
-    storage = multer.diskStorage({
-        destination: (req, file, callback) => {
-          callback(null, 'images');
-        },
-        filename: (req, file, callback) => {
-          const name = file.originalname.split(' ').join('_');
-          const extension = MIME_TYPES[file.mimetype];
-          callback(null, name + Date.now() + '.' + extension);
-        }
-    });
+try {
+    if(!fs.existsSync(dir)) {
+        fs.mkdir(dir, { recursive: true }, function(err) {
+            if (err) {
+                console.log(err)
+            } else {
+                storage = multer.diskStorage({
+                    destination: (req, file, callback) => {
+                        callback(null, './public/posts/images');
+                    },
+                    filename: (req, file, callback) => {
+                        const name = file.originalname.split(' ').join('_');
+                        const extension = MIME_TYPES[file.mimetype];
+                        callback(null, name + Date.now() + '.' + extension);
+                    }
+                });
+                console.log("New directory successfully created.");
+            }
+        })
+    } else {
+        storage = multer.diskStorage({
+            destination: (req, file, callback) => {
+              callback(null, './public/posts/images');
+            },
+            filename: (req, file, callback) => {
+              const name = file.originalname.split(' ').join('_');
+              const extension = MIME_TYPES[file.mimetype];
+              callback(null, name + Date.now() + '.' + extension);
+            }
+        });
+    }
+} catch (error) {
+    console.log(error);
 }
-
 
 export default  multer({storage: storage}).single('image');
