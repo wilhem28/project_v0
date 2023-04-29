c3bebbcfe21823825208955353059481da685d75
Wilhem Nédan <wilhem.nedan@gmail.com>
2023-04-28 22:05:03 -0400

    Improve thme multer-config file

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,21 +1,42 @@
 import multer from 'multer';
+import fs from 'fs';
+
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png',
   'image/webp': 'webp'
-};
+},
+dir = './images'
+;
+let storage = "";
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'public/posts/single');
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
-});
+
+if(!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+    storage = multer.diskStorage({
+        destination: (req, file, callback) => {
+            callback(null, 'images');
+        },
+        filename: (req, file, callback) => {
+            const name = file.originalname.split(' ').join('_');
+            const extension = MIME_TYPES[file.mimetype];
+            callback(null, name + Date.now() + '.' + extension);
+        }
+    });
+} else {
+    storage = multer.diskStorage({
+        destination: (req, file, callback) => {
+          callback(null, 'images');
+        },
+        filename: (req, file, callback) => {
+          const name = file.originalname.split(' ').join('_');
+          const extension = MIME_TYPES[file.mimetype];
+          callback(null, name + Date.now() + '.' + extension);
+        }
+    });
+}
+
 
 export default  multer({storage: storage}).single('image');
