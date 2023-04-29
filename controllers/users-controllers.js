d64ac6654d38c83bacbb6e8e81e19ec5588b5893
Wilhem Nédan <wilhem.nedan@gmail.com>
2023-04-29 14:44:38 -0400

    Validate name, city, password user route

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,5 +1,6 @@
 import pool from '../db.js';
 import bcrypt from 'bcrypt';
+import { validate } from 'email-validator';
 import queries from '../queries/queries.js';
 import ClassUser from '../classes/class-user.js';
 import ClassAdmin from '../classes/class-admin.js';
@@ -28,30 +29,50 @@ const getUserById = async(req,res) => {
 const createUser = async(req,res) => {
     try {
         const {name,email,password,privileges,genre,dob,city} = req.body;
-        const profilImg = req.file.filename; 
-        const newClassAdmin = new ClassAdmin(name,email,password,privileges,genre,dob,city,profilImg);
-        const newClassUser = new ClassUser(name,email,password,genre,dob,city,profilImg);
+        const profilImg = req.file.filename;
 
-        const userAlreadyExists = await pool.query(queries.logUser,[email]);
+        const regex = new RegExp(/^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]{3,}$/u); 
+        const regexPassword = new RegExp(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/); 
+        const validateName = regex.test(name);
+        const validateCity = regex.test(city);
+        const validatePassword = regexPassword.test(password);
 
-        if(userAlreadyExists.rowCount == 1) {
-            res.json("Il existe déjà un utilisateur utilisant cet email. Veuillez essayer avec un autre email.");
-        } else {
-            const users = await pool.query(queries.selectUsers);
-    
-            if(users.rowCount === 0) {
-                const booleanValue = true;
-                const hashedPassword = await bcrypt.hash(newClassAdmin.password,10);
-                const newUser = await pool.query(queries.createAdmin,[newClassAdmin.name,newClassAdmin.email,hashedPassword,booleanValue,newClassAdmin.genre,newClassAdmin.dob,newClassAdmin.city,newClassAdmin.img]);
-                res.json({users:newUser.rows[0]});
-            } else {
-                const booleanValuefalse = false;
-                const hashedPassword = await bcrypt.hash(newClassUser.password,10);
-                const newUser = await pool.query(queries.createUser,[newClassUser.name,newClassUser.email,hashedPassword,newClassUser.genre,newClassUser.dob,newClassUser.city,newClassUser.img]);
-                res.json({users:newUser.rows[0]});
-            }
+        if (!validateName) {
+            return res.json("Votre nom doit comporter uniquement des lettres avec un minimum de trois lettres.");
         }
-
+        if (!validateCity) {
+            return res.json("Le nom de votre lieu de résidence doit être composé uniquement de lettres avec un minimum de 3 lettres.");
+        }
+        if (!validatePassword) {
+            return res.json("Votre password doit vérifier les conditions suivantes : 8 caractères minimum, 1 lettre majuscule minimum, 1 lettre miniscule minimum, 1 chiffre minimum, et 1 caractère spécial minimum");
+        }
+        if (validate(email)) {
+            const newClassAdmin = new ClassAdmin(name,email,password,privileges,genre,dob,city,profilImg);
+            const newClassUser = new ClassUser(name,email,password,genre,dob,city,profilImg);
+    
+            const userAlreadyExists = await pool.query(queries.logUser,[email]);
+           
+            if(userAlreadyExists.rowCount == 1) {
+                res.json("Il existe déjà un utilisateur utilisant cet email. Veuillez essayer avec un autre email.");
+            } 
+            else {
+                const users = await pool.query(queries.selectUsers);
+        
+                if(users.rowCount === 0) {
+                    const booleanValue = true;
+                    const hashedPassword = await bcrypt.hash(newClassAdmin.password,10);
+                    const newUser = await pool.query(queries.createAdmin,[newClassAdmin.name,newClassAdmin.email,hashedPassword,booleanValue,newClassAdmin.genre,newClassAdmin.dob,newClassAdmin.city,newClassAdmin.img]);
+                    res.json({users:newUser.rows[0]});
+                } else {
+                    const booleanValuefalse = false;
+                    const hashedPassword = await bcrypt.hash(newClassUser.password,10);
+                    const newUser = await pool.query(queries.createUser,[newClassUser.name,newClassUser.email,hashedPassword,newClassUser.genre,newClassUser.dob,newClassUser.city,newClassUser.img]);
+                    res.json({users:newUser.rows[0]});
+                }
+            }
+        } else {
+            res.json("Le format de votre email semble incorrect.");
+        }
     } catch (error) {
         res.status(500).json({error:error.message});
     }
